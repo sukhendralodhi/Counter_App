ab313003e24839dda92b9da262d107602c36a7e0
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-15 20:43:24 +0000

    update JS

--- script.js
+++ script.js
@@ -1,32 +1,60 @@
 const counter = document.querySelector("span");
+const buttons = document.querySelector(".buttons");
 
 const sub = document.getElementById("sub");
 const res = document.getElementById("res");
 const add = document.getElementById("add");
 
-sub.addEventListener("click", () => {
-  counter.innerText--;
-  if (counter.innerText < 0) {
-    counter.classList.add("text-red-800");
-    counter.classList.remove("text-black");
-    counter.classList.remove("text-green-900");
+buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("sub")) {
+    counter.innerText--;
+    setColor();
+  }
+  if (e.target.innerText === "Add") {
+    counter.innerText++;
+    setColor();
+  }
+
+  if (e.target.innerText === "Reset") {
+    counter.innerText = 0;
+    setColor();
   }
 });
 
-res.addEventListener("click", () => {
-  counter.innerText = 0;
-  if (counter.innerText == 0) {
-    counter.classList.add("text-black");
-    counter.classList.remove("text-green-900");
-    counter.classList.remove("text-red-800");
-  }
-});
-
-add.addEventListener("click", () => {
-  counter.innerText++;
+function setColor() {
   if (counter.innerText > 0) {
-    counter.classList.add("text-green-900");
-    counter.classList.remove("text-black");
-    counter.classList.remove("text-red-800");
+    counter.classList.toggle("text-yellow-900");
+  } else if (counter.innerText < 0) {
+    counter.classList.toggle("text-red-900");
+  } else {
+    counter.classList.toggle("text-white");
   }
-});
+}
+
+
+// sub.addEventListener("click", () => {
+//   counter.innerText--;
+//   if (counter.innerText < 0) {
+//     counter.classList.add("text-red-800");
+//     counter.classList.remove("text-black");
+//     counter.classList.remove("text-green-900");
+//   }
+// });
+
+// res.addEventListener("click", () => {
+//   counter.innerText = 0;
+//   if (counter.innerText == 0) {
+//     counter.classList.add("text-black");
+//     counter.classList.remove("text-green-900");
+//     counter.classList.remove("text-red-800");
+//   }
+// });
+
+// add.addEventListener("click", () => {
+//   counter.innerText++;
+//   if (counter.innerText > 0) {
+//     counter.classList.add("text-green-900");
+//     counter.classList.remove("text-black");
+//     counter.classList.remove("text-red-800");
+//   }
+// });
